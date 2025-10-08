fb2b3f57f62a9a13df3434949420dd2328ebd770
KyeeeSec <179015305+KyeeeSec@users.noreply.github.com>
2025-10-08 20:45:18 +0100

    fully completed, integrated with SupaBase DB

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, request, jsonify, render_template
+import os
+import random
+from supabase import create_client, Client
+from dotenv import load_dotenv
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
 @app.route('/')
 def main():
-    return render_template('index.html')
+
+    notes = supabase.table("notes").select("text").execute()
+    print(notes)
+    notes = notes.data
+    return render_template('index.html', notes=notes)
 @app.route('/submit-note', methods=['POST'])
 def submit_note():
     data = request.get_json()
     note = data.get('note')
+    response = supabase.table("notes").insert({
+        "id": random.randint(0, 99999),
+        "text": note
+    }).execute()
+    print(response)
+
     print("Adding", note)
     return jsonify({"status": "success"}), 200
 
