f9f65b33fea0f54bc2cdf8877ed73a967d6c480d
KyeeeSec <kyeeetypesfast@gmail.com>
2025-10-27 18:52:37 +0000

    Changed styling for a modern look

--- static/main.js
+++ static/main.js
@@ -1,8 +1,10 @@
 let notesList = document.getElementById('notesList');
 let note = document.getElementById('note');
 let button = document.getElementById('notebtn');
+let btn1 = document.getElementById('btn1')
+let body = document.getElementsByClassName('body')
 
-
+let boolDark = true;
 if (button) {
     let trimmedText = note.value.trim();
     console.log(trimmedText); 
@@ -24,3 +26,17 @@ if (button) {
         }
     }
 )}
+if(btn1) {
+    btn1.addEventListener("click", (event) => {
+        console.log("a");
+        btn1.id = "btn2";
+        boolDark = !boolDark;
+        console.log(boolDark);
+        if(boolDark == true) {
+            body.id = "body";
+        
+        } else if(boolDark == false) {
+            body.id = "body2";
+        }
+        
+    })}
